ef7b9cb87695deff71602be6efc6b416931cf7b7
Krishna Kumar <krishnakumar750514@gmail.com>
2024-07-13 12:25:48 +0530

    update index.jsx

---
+++ color_generator/src/components/index.jsx
@@ -0,0 +1,11 @@
+export default function  Random() {
+    return (
+        <>
+            <div className="container">
+                <button>Generate RGB Color</button>
+                <button>Generate HEX Color</button>
+                <button>Generate Random Color</button>
+            </div>
+        </>
+    );
+}
